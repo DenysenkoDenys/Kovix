2503bf7cdc132604ada4deae0c81c6c1fe8c6e3e
Денисенко Денис Олександрович <ipz224_ddo@student.ztu.edu.ua>
2026-05-12 15:40:07 +0300

    Fix pipeline

--- Movie.API/obj/Debug/net8.0/Movie.API.AssemblyInfo.cs
+++ Movie.API/obj/Debug/net8.0/Movie.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Movie.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c9a9368bc19fa3f09b401627a4b23226aa6508c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9582d7ac5cb27d55732e369b31758b1c2f9e6e3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Movie.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Movie.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
